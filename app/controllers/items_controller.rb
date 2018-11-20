a46b96b6a656b9bf2e0ee6e436f9721afd48d526
Ellen Wendelin <ellenwendelin@live.se>
2018-11-20 20:18:28 +0800

    index, show and create working and tested

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,10 +1,29 @@
 class ItemsController < ApplicationController
   def index
-    @items = Item.all
+    @items = Item.all # add only the items that are avaliable for renting
+    itemsArray = @items.to_a
+    itemsArray.select! do |item|
+      item.order.nil?
+    end
+    render json: {
+      items: itemsArray
+    }
   end
 
   def show
-    @item = Item.find(params[:id])
+    @item = Item.find_by(id: params[:id])
+    unless @item.nil?
+      render json: {
+      item: @item
+      }
+    else
+      render json: {
+      error: {
+        code: 2,
+        message: "Item not found"
+      }
+    }
+    end
   end
 
   def create
@@ -25,17 +44,24 @@ class ItemsController < ApplicationController
   end
 
   def renting
+    # the list of items that i am currently renting
 
   end
 
   def rentingout
-
+    # the list of items that i am currently renting out / that i posted
+    # the items i have created should go in here
+    # testie = User.find_by(access_token: params[:access_token])
+    # testie.items
   end
 
   def update
+    # when i click on the edit icon on an item in rentingout i get redirected to the page that is edit and when i click submit it should be changed
   end
 
   def destroy
+    # when you click on delete icon on an item in rentingout i delete it immediate
+
   end
 
   private
--- db/seeds.rb
+++ db/seeds.rb
@@ -4,4 +4,14 @@
 # Examples:
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+#   Character.create(name: 'Luke', movie: movies.firs
+Order.destroy_all
+User.destroy_all
+Item.destroy_all
+
+u = User.create()
+i = u.items.create(name: "Ellen", price: 100)
+# Item.create(name: "Ellen", price: 100, user: u)
+o = Order.create(user_id: u.id, item_id: i.id)
+
+# o = Order.create(user: u, item: i)
