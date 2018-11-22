8f2d82d9020777ac464cf43ccad128aeed002278
Ellen Wendelin <ellenwendelin@live.se>
2018-11-22 15:34:21 +0800

    added reviews model and contoller

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,8 +2,7 @@ class Order < ApplicationRecord
   belongs_to :user # buyer
   belongs_to :item # seller
   has_one :owner, through: :items, class_name: "User" # seller
-  has_one :end_time
-  has_one :start_time
+  has_many :reviews
   validates :start_time, presence: true
   validates :end_time, presence: true
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,5 @@ class User < ApplicationRecord
   has_many :items
   has_many :orders
   has_many :borrowed_items, through: :orders, class_name: "Item"
+  has_many :reviews
 end
